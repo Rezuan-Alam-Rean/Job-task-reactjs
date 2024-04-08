6aeb6414ed8623a480a38c05979295fd8c3279d1
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-04-08 20:57:40 +0600

    init project

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+
+const Dashboard = () => {
+    return (
+        <div>
+            <div role="tablist" className="tabs tabs-bordered">
+  <a role="tab" className="tab">Tab 1</a>
+  <a role="tab" className="tab tab-active">Tab 2</a>
+  <a role="tab" className="tab">Tab 3</a>
+</div>
+        </div>
+    );
+};
+
+export default Dashboard;
