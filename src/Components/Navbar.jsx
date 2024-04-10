5c0f3fe2f0068576136e8517e24d4de3aa9e9610
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-04-10 23:22:19 +0600

    complete project

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,73 +4,55 @@ import { IoFlashOutline } from "react-icons/io5";
 const Navbar = () => {
     return (
         <div>
-
-
-            <header className="bg-orange-100 w-full  md:px-6 p-2">
-                <div className="flex items-center gap-x-4" >
-                    <div className="flex px-2  bg-orange-300 p-1  rounded-full items-center  ">
-                        <div>
-                            <IoIosTime />
-                        </div>
-                        <p className="px-2 font-bold text-sm text-slate-700">One-Time Offer</p>
+            <header className="bg-orange-100 w-full px-2 sm:px-6 p-2 md:flex sm:flex  ">
+                <div className="flex px-2 bg-orange-300 p-1 justify-center rounded-full items-center md:mr-4 ">
+                    <div>
+                        <IoIosTime />
                     </div>
-                    <div> Your Ai StoryBoarding Bundle Discount expires in <span className="text-red-400 px-1 ml-2 bg-white">5:01</span> </div>
-                    <div> <button className="btn px-4  bg-red-200 md:btn-sm">Claim Discount</button> </div>
-
-
+                    <p className="px-2 font-bold text-sm text-slate-700">One-Time Offer</p>
                 </div>
+                <div className="  flex  sm:flex-row items-center gap-2 sm:gap-x-4">
 
+                    <div className="text-center sm:text-left"> Your Ai StoryBoarding Bundle Discount expires in <span className="text-red-400 px-1 ml-2 bg-white">5:01</span> </div>
+                    <div className="mt-2 sm:mt-0"> <button className="btn px-4 bg-red-200 md:btn-sm">Claim Discount</button> </div>
+                </div>
             </header>
 
-
-            <div className="navbar bg-white py-5 shadow-md justify-between  ">
-                <div className="flex ml-4  ">
-                    <button className=" rounded-full w-10 h-10 mr-4  bg-black text-white font-extrabold text-2xl">
+            <div className="navbar bg-white py-2 sm:py-5 shadow-md justify-between">
+                <div className="flex ml-2 sm:ml-4">
+                    <button className="rounded-full w-8 h-8 sm:w-10 sm:h-10 mr-2 sm:mr-4 bg-black text-white font-extrabold text-xl sm:text-2xl">
                         B
                     </button>
-                    <h3 className="text-black text-3xl font-bold ">Project Dashboard</h3>
-
+                    <h3 className="text-black text-2xl sm:text-3xl font-bold">Project Dashboard</h3>
                 </div>
 
-                <div className="gap-x-4  " >
-                    <div className=" hidden  md:flex gap-2 relative">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute ml-3 mt-4 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                        <div className="form-control  ">
-
-                            <input type="text" placeholder="Search My Storyboards" className="input  border-slate-950 w-24 px-2 md:w-72 md:px-10 " />
-
+                <div className={`gap-x-2 sm:gap-x-4 mt-2 sm:mt-0`}>
+                    <div className=" hidden md:flex gap-2 relative">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 absolute ml-2 sm:ml-3 mt-3 sm:mt-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        <div className="form-control">
+                            <input type="text" placeholder="Search My Storyboards" className="input border-slate-950 w-20 sm:w-24 px-1 sm:px-2 md:w-72 md:px-10" />
                         </div>
-
                     </div>
 
-
-                    <div className=" hidden md:flex items-center bg-sky-50 gap-x-3 px-3 p-1 border-2 border-sky-200">
-                        <button className=" border-sky-300 font-bold text-xs border-4 rounded-full w-8 h-8 ">
-
+                    <div className=" hidden  md:flex items-center bg-sky-50 gap-x-2 sm:gap-x-3 px-2 sm:px-3 py-1 border-2 border-sky-200">
+                        <button className="border-sky-300 font-bold text-xs border-4 rounded-full w-6 h-6 sm:w-8 sm:h-8">
                             4
-
                         </button>
                         <p className="font-bold text-black">Quick Start</p>
                     </div>
 
-                    <div>
-                        <button className=" hidden md:block font-extrabold text-lg border-2 border-black rounded-full w-10 h-10 bg-orange-200 " >A</button>
+                    <div className="mt-2 sm:mt-0">
+                        <button className="font-extrabold text-lg border-2 border-black rounded-full w-8 h-8 sm:w-10 sm:h-10 bg-orange-200">A</button>
                     </div>
 
-                    <div>
-                        <button className="h-10 w-10 bg-black rounded-s-full px-3 text-white" ><IoFlashOutline size={22} />
+                    <div className="mt-2 sm:mt-0">
+                        <button className="h-8 w-8 sm:h-10 sm:w-10 bg-black rounded-s-full px-2 sm:px-3 text-white"><IoFlashOutline size={18} sm:size={22} />
                         </button>
-
                     </div>
                 </div>
-
             </div>
-
         </div>
     );
 };
 
 export default Navbar;
-
-
-
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,75 +1,147 @@
+import { useState } from 'react';
 import { PiFolderOpen } from "react-icons/pi";
 import { IoEllipsisVerticalSharp } from "react-icons/io5";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { IoAddOutline } from "react-icons/io5";
 
-
 const Projects = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className="bg-slate-100" >
-            <div className="flex justify-between pt-10 px-10 " >
-                <div className="flex gap-x-2 items-center " >
-                    <button className=" font-extrabold text-lg border-2 border-black rounded-full w-10 h-10 bg-orange-200 " >A</button>
-                    <p className="font-extrabold text-xl text-black" >Adam Cooper Team</p>
+        <div className="bg-slate-100 pb-10">
+            <div className="flex flex-col sm:flex-row justify-between pt-4  sm:pt-10 px-4 sm:px-10">
+                <div className="flex gap-x-2 items-center">
+                    <button className="font-extrabold text-lg border-2 border-black rounded-full w-8 h-8 sm:w-10 sm:h-10 bg-orange-200">A</button>
+                    <p className="font-extrabold text-xl text-black">Adam Cooper Team</p>
                 </div>
 
-                <div>
-                    <button className="btn btn-sm bg-black text-white">New Project</button>
+                <div className="mt-2 sm:mt-0">
+
+
+
+                    <div className="">
+                        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                        <button className="btn btn-sm bg-black text-white" onClick={() => document.getElementById('my_modal_3').showModal()}>New Project</button>
+                        <dialog id="my_modal_3" className="modal">
+                            <div className="modal-box">
+                                <form method="dialog">
+                                    {/* if there is a button in form, it will close the modal */}
+                                    <button className="btn btn-sm btn-circle rounded-full bg-slate-200 btn-ghost absolute right-2 top-4">✕</button>
+                                </form>
+                                <h3 className="font-bold text-xl">New Storyboard</h3>
+                                <label className="form-control w-full">
+                                    <div className="label">
+                                        <span className="label-text">Storyboard Name</span>
+                                    </div>
+                                    <input type="text" placeholder="e.g. Default project Storyboard" className="input input-bordered w-full" />
+                                    <div className="label">
+                                        <span className="label-text">Frame Size</span>
+                                    </div>
+                                    <select className="select select-bordered join-item w-full">
+                                        <option disabled selected>Landscape (16:9)</option>
+                                        <option>Sci-fi</option>
+                                        <option>Drama</option>
+                                        <option>Action</option>
+                                    </select>
+                                </label>
+                                <hr className="mt-6" />
+
+                                <div className="flex justify-between mt-6">
+                                    <div className="flex items-center gap-x-2">
+                                        <input type="checkbox" className="toggle toggle-xs" />
+                                        <p className="text-black">Ai Assistant</p>
+                                    </div>
+
+                                    <button className="btn btn-sm">Create Storyboard</button>
+                                </div>
+                            </div>
+                        </dialog>
+                    </div>
+
+
+
+
+
+
                 </div>
             </div>
 
-
-            <div className="mt-6 mx-10 bg-white py-10" >
-                <div className="justify-between  px-10  flex  items-center" >
-                    <div className="flex gap-x-4 items-center">
+            <div className="mt-4 sm:mt-6 mx-4 sm:mx-10 bg-white py-4 sm:py-10">
+                <div className="justify-between px-4 sm:px-10 flex flex-col sm:flex-row items-center">
+                    <div className="flex gap-x-4 items-center mt-2 sm:mt-0">
                         <div>
                             <PiFolderOpen size={35} />
                         </div>
                         <div>
-                            <p className="text-black text-lg font-bold " >Default Project</p>
-                            <p className=" mt-1 text-gray-600" > 1 Storyboard</p>
+                            <p className="text-black text-lg font-bold">Default Project</p>
+                            <p className="mt-1 text-gray-600">1 Storyboard</p>
                         </div>
                     </div>
 
-                    <div className="flex gap-x-8" >
+                    <div className="flex gap-x-4 mt-2 sm:mt-0">
                         <div>
                             <IoEllipsisVerticalSharp />
-
                         </div>
 
                         <div>
                             <IoChevronDownOutline />
-
                         </div>
                     </div>
                 </div>
 
-
-                <div className="md:flex gap-x-6 mt-6 px-6" >
+                <div className="flex flex-col md:flex-row gap-y-4 md:gap-x-6 mt-4 sm:mt-6 px-4 sm:px-6">
                     <div>
-                        <div className=" card card-compact w-96 bg-slate-100 h-80 border-black border ">
+                        <div className="card card-compact w-full sm:w-96 bg-slate-100 h-80 border-black border">
                             <figure><img src="https://img.freepik.com/premium-photo/black-white-jungle-wallpaper-garden-background_906149-5113.jpg" alt="jungle" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Shoes!</h2>
                                 <p>If a dog chews shoes whose shoes does he choose?</p>
-
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className=" mt-4 md:mt-0 card card-compact w-96 h-80 bg-slate-100 items-center justify-center  ">
-                            <div className="" >
-                                <button className=" text-black font-extrabold btn btn-circle btn-outline">
-                                <IoAddOutline size={22} />
+                        <div className="mt-4 md:mt-0 card card-compact w-full sm:w-96 h-80 bg-slate-100 items-center justify-center">
+                            <div className="">
+                                {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                                <button className="text-black font-extrabold btn btn-circle btn-outline" onClick={() => document.getElementById('my_modal_3').showModal()}><IoAddOutline size={22} /></button>
+                                <dialog id="my_modal_3" className="modal">
+                                    <div className="modal-box">
+                                        <form method="dialog">
+                                            {/* if there is a button in form, it will close the modal */}
+                                            <button className="btn btn-sm btn-circle rounded-full bg-slate-200 btn-ghost absolute right-2 top-4">✕</button>
+                                        </form>
+                                        <h3 className="font-bold text-xl">New Storyboard</h3>
+                                        <label className="form-control w-full">
+                                            <div className="label">
+                                                <span className="label-text">Storyboard Name</span>
+                                            </div>
+                                            <input type="text" placeholder="e.g. Default project Storyboard" className="input input-bordered w-full" />
+                                            <div className="label">
+                                                <span className="label-text">Frame Size</span>
+                                            </div>
+                                            <select className="select select-bordered join-item w-full">
+                                                <option disabled selected>Landscape (16:9)</option>
+                                                <option>Sci-fi</option>
+                                                <option>Drama</option>
+                                                <option>Action</option>
+                                            </select>
+                                        </label>
+                                        <hr className="mt-6" />
 
-                                </button>
+                                        <div className="flex justify-between mt-6">
+                                            <div className="flex items-center gap-x-2">
+                                                <input type="checkbox" className="toggle toggle-xs" />
+                                                <p className="text-black">Ai Assistant</p>
+                                            </div>
 
+                                            <button className="btn btn-sm">Create Storyboard</button>
+                                        </div>
+                                    </div>
+                                </dialog>
                             </div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
         </div>
     );
